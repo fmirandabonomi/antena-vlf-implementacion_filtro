87f8da4e2c4a938b1329cdef19ed3bb99e933fd1
Fernando Alberto Miranda Bonomi <fmirandabonomi@herrera.unt.edu.ar>
2025-06-23 16:31:37 -0300

    Bloques para NCO

--- inc/blocks.h
+++ inc/blocks.h
@@ -1,5 +1,6 @@
 #ifndef BLOCKS_H
 #define BLOCKS_H
+#include <math.h>
 #include <stdint.h>
 
 #ifdef TRACE_EXEC
@@ -55,5 +56,55 @@ inline static int32_t comb_14d(int32_t input, int32_t (*mem)[16], uint8_t *phase
     SHIFT_IN(input, *phase, *mem, 16);
     return output;
 }
+
+#ifndef PI
+#define PI 3.14159265358979323846
+#endif
+#ifndef AMPLITUDE
+#define AMPLITUDE INT16_MAX
+#endif
+#ifndef NCO_GUARD_BITS
+#define NCO_GUARD_BITS 3
+#endif
+#define NCO_STATE enum { COEF_REAL, COEF_IMAG, STATE_REAL, STATE_IMAG }
+
+inline static void nco_init(int32_t (*mem)[4], double outFreq, double sampFreq)
+{
+    NCO_STATE;
+    (*mem)[COEF_REAL]  = (int32_t)((1 << 30) * cos(-2 * PI * outFreq / sampFreq));
+    (*mem)[COEF_IMAG]  = (int32_t)((1 << 30) * sin(-2 * PI * outFreq / sampFreq));
+    (*mem)[STATE_REAL] = (int32_t)INT16_MAX << NCO_GUARD_BITS;
+    (*mem)[STATE_IMAG] = 0;
+}
+#define MUL_I64(a, b) ((int64_t)(a) * (int64_t)(b))
+
+inline static void nco_sample(int32_t (*mem)[4], int16_t (*cplx)[2])
+{
+    NCO_STATE;
+    int32_t sta_r = (*mem)[STATE_REAL] >> NCO_GUARD_BITS;
+    int32_t sta_i = (*mem)[STATE_IMAG] >> NCO_GUARD_BITS;
+    if (!sta_i || sta_r > AMPLITUDE || sta_r < -AMPLITUDE) {
+        sta_r              = sta_r >= 0 ? AMPLITUDE : -AMPLITUDE;
+        sta_i              = 0;
+        (*mem)[STATE_REAL] = sta_r << NCO_GUARD_BITS;
+        (*mem)[STATE_IMAG] = 0;
+    } else if (!sta_r || sta_i > AMPLITUDE || sta_r < -AMPLITUDE) {
+        sta_r              = 0;
+        sta_i              = sta_i >= 0 ? AMPLITUDE : -AMPLITUDE;
+        (*mem)[STATE_REAL] = 0;
+        (*mem)[STATE_IMAG] = sta_i << NCO_GUARD_BITS;
+    }
+    (*cplx)[0] = sta_r;
+    (*cplx)[1] = sta_i;
+    (*mem)[STATE_REAL] =
+        (int32_t)((MUL_I64((*mem)[COEF_REAL], (*mem)[STATE_REAL]) - MUL_I64((*mem)[COEF_IMAG], (*mem)[STATE_IMAG]))
+                  >> 30);
+    (*mem)[STATE_IMAG] =
+        (int32_t)((MUL_I64((*mem)[COEF_IMAG], (*mem)[STATE_REAL]) + MUL_I64((*mem)[COEF_REAL], (*mem)[STATE_IMAG]))
+                  >> 30);
+}
+#undef NCO_STATE
+#undef MUL_I64
+
 #endif // BLOCKS_H
 
